3ec5da499c04c06d1f88a12e4f0f6cee43219fd8
Christopher <christophervistal24@gmail.com>
2018-05-09 19:28:13 +0800

    REVISION PRINT TABLE

--- App/Views/admin/print.php
+++ App/Views/admin/print.php
@@ -1,5 +1,6 @@
 <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/system/App/init.php'; ?>
 <?php
+date_default_timezone_set('Asia/Manila');
 use App\Model\User;
 $db = (new User)->connect();
 $result = $db->query("
@@ -17,6 +18,7 @@ SELECT
     `entrace_rating`.`verbal_total`,
     `entrace_rating`.`non_verbal_total`,
     `entrace_rating`.`over_all_total`,
+    `admission_result`.`guidance_counselor`,
     `course`.`course` as first_course,
     `course_2`.`course` as second_course
 FROM
@@ -30,8 +32,290 @@ ON
 WHERE
     admission_result.id = ' $_GET[id] '
 ")->fetch(PDO::FETCH_ASSOC);
- class PDF extends FPDF
+
+//function hex2dec
+//returns an associative array (keys: R,G,B) from a hex html code (e.g. #3FE5AA)
+function hex2dec($couleur = "#000000"){
+    $R = substr($couleur, 1, 2);
+    $rouge = hexdec($R);
+    $V = substr($couleur, 3, 2);
+    $vert = hexdec($V);
+    $B = substr($couleur, 5, 2);
+    $bleu = hexdec($B);
+    $tbl_couleur = array();
+    $tbl_couleur['R']=$rouge;
+    $tbl_couleur['G']=$vert;
+    $tbl_couleur['B']=$bleu;
+    return $tbl_couleur;
+}
+
+//conversion pixel -> millimeter in 72 dpi
+function px2mm($px){
+    return $px*25.4/72;
+}
+
+function txtentities($html){
+    $trans = get_html_translation_table(HTML_ENTITIES);
+    $trans = array_flip($trans);
+    return strtr($html, $trans);
+}
+
+class PDF extends FPDF
 {
+//variables of html parser
+protected $B;
+protected $I;
+protected $U;
+protected $HREF;
+protected $fontList;
+protected $issetfont;
+protected $issetcolor;
+function __construct($orientation='P', $unit='mm', $format='A4')
+{
+    //Call parent constructor
+    parent::__construct($orientation,$unit,$format);
+
+    //Initialization
+    $this->B=0;
+    $this->I=0;
+    $this->U=0;
+    $this->HREF='';
+
+    $this->tableborder=0;
+    $this->tdbegin=false;
+    $this->tdwidth=0;
+    $this->tdheight=0;
+    $this->tdalign="L";
+    $this->tdbgcolor=false;
+
+    $this->oldx=0;
+    $this->oldy=0;
+
+    $this->fontlist=array("arial","times","courier","helvetica","symbol");
+    $this->issetfont=false;
+    $this->issetcolor=false;
+}
+
+//////////////////////////////////////
+//html parser
+
+function WriteHTML($html)
+{
+    $html=strip_tags($html,"<b><u><i><a><img><p><br><strong><em><font><tr><blockquote><hr><td><tr><table><sup>"); //remove all unsupported tags
+    $html=str_replace("\n",'',$html); //replace carriage returns with spaces
+    $html=str_replace("\t",'',$html); //replace carriage returns with spaces
+    $a=preg_split('/<(.*)>/U',$html,-1,PREG_SPLIT_DELIM_CAPTURE); //explode the string
+    foreach($a as $i=>$e)
+    {
+        if($i%2==0)
+        {
+            //Text
+            if($this->HREF)
+                $this->PutLink($this->HREF,$e);
+            elseif($this->tdbegin) {
+                if(trim($e)!='' && $e!="&nbsp;") {
+                    $this->Cell($this->tdwidth,$this->tdheight,$e,$this->tableborder,'',$this->tdalign,$this->tdbgcolor);
+                }
+                elseif($e=="&nbsp;") {
+                    $this->Cell($this->tdwidth,$this->tdheight,'',$this->tableborder,'',$this->tdalign,$this->tdbgcolor);
+                }
+            }
+            else
+                $this->Write(5,stripslashes(txtentities($e)));
+        }
+        else
+        {
+            //Tag
+            if($e[0]=='/')
+                $this->CloseTag(strtoupper(substr($e,1)));
+            else
+            {
+                //Extract attributes
+                $a2=explode(' ',$e);
+                $tag=strtoupper(array_shift($a2));
+                $attr=array();
+                foreach($a2 as $v)
+                {
+                    if(preg_match('/([^=]*)=["\']?([^"\']*)/',$v,$a3))
+                        $attr[strtoupper($a3[1])]=$a3[2];
+                }
+                $this->OpenTag($tag,$attr);
+            }
+        }
+    }
+}
+
+function OpenTag($tag, $attr)
+{
+    //Opening tag
+    switch($tag){
+
+        case 'SUP':
+            if( !empty($attr['SUP']) ) {
+                //Set current font to 6pt
+                $this->SetFont('','',6);
+                //Start 125cm plus width of cell to the right of left margin
+                //Superscript "1"
+                $this->Cell(2,2,$attr['SUP'],0,0,'L');
+            }
+            break;
+
+        case 'TABLE': // TABLE-BEGIN
+            if( !empty($attr['BORDER']) ) $this->tableborder=$attr['BORDER'];
+            else $this->tableborder=0;
+            break;
+        case 'TR': //TR-BEGIN
+            break;
+        case 'TD': // TD-BEGIN
+            if( !empty($attr['WIDTH']) ) $this->tdwidth=($attr['WIDTH']/4);
+            else $this->tdwidth=40; // Set to your own width if you need bigger fixed cells
+            if( !empty($attr['HEIGHT']) ) $this->tdheight=($attr['HEIGHT']/6);
+            else $this->tdheight=6; // Set to your own height if you need bigger fixed cells
+            if( !empty($attr['ALIGN']) ) {
+                $align=$attr['ALIGN'];
+                if($align=='LEFT') $this->tdalign='L';
+                if($align=='CENTER') $this->tdalign='C';
+                if($align=='RIGHT') $this->tdalign='R';
+            }
+            else $this->tdalign='L'; // Set to your own
+            if( !empty($attr['BGCOLOR']) ) {
+                $coul=hex2dec($attr['BGCOLOR']);
+                    $this->SetFillColor($coul['R'],$coul['G'],$coul['B']);
+                    $this->tdbgcolor=true;
+                }
+            $this->tdbegin=true;
+            break;
+
+        case 'HR':
+            if( !empty($attr['WIDTH']) )
+                $Width = $attr['WIDTH'];
+            else
+                $Width = $this->w - $this->lMargin-$this->rMargin;
+            $x = $this->GetX();
+            $y = $this->GetY();
+            $this->SetLineWidth(0.2);
+            $this->Line($x,$y,$x+$Width,$y);
+            $this->SetLineWidth(0.2);
+            $this->Ln(1);
+            break;
+        case 'STRONG':
+            $this->SetStyle('B',true);
+            break;
+        case 'EM':
+            $this->SetStyle('I',true);
+            break;
+        case 'B':
+        case 'I':
+        case 'U':
+            $this->SetStyle($tag,true);
+            break;
+        case 'A':
+            $this->HREF=$attr['HREF'];
+            break;
+        case 'IMG':
+            if(isset($attr['SRC']) && (isset($attr['WIDTH']) || isset($attr['HEIGHT']))) {
+                if(!isset($attr['WIDTH']))
+                    $attr['WIDTH'] = 0;
+                if(!isset($attr['HEIGHT']))
+                    $attr['HEIGHT'] = 0;
+                $this->Image($attr['SRC'], $this->GetX(), $this->GetY(), px2mm($attr['WIDTH']), px2mm($attr['HEIGHT']));
+            }
+            break;
+        case 'BLOCKQUOTE':
+        case 'BR':
+            $this->Ln(5);
+            break;
+        case 'P':
+            $this->Ln(10);
+            break;
+        case 'FONT':
+            if (isset($attr['COLOR']) && $attr['COLOR']!='') {
+                $coul=hex2dec($attr['COLOR']);
+                $this->SetTextColor($coul['R'],$coul['G'],$coul['B']);
+                $this->issetcolor=true;
+            }
+            if (isset($attr['FACE']) && in_array(strtolower($attr['FACE']), $this->fontlist)) {
+                $this->SetFont(strtolower($attr['FACE']));
+                $this->issetfont=true;
+            }
+            if (isset($attr['FACE']) && in_array(strtolower($attr['FACE']), $this->fontlist) && isset($attr['SIZE']) && $attr['SIZE']!='') {
+                $this->SetFont(strtolower($attr['FACE']),'',$attr['SIZE']);
+                $this->issetfont=true;
+            }
+            break;
+    }
+}
+
+function CloseTag($tag)
+{
+    //Closing tag
+    if($tag=='SUP') {
+    }
+
+    if($tag=='TD') { // TD-END
+        $this->tdbegin=false;
+        $this->tdwidth=0;
+        $this->tdheight=0;
+        $this->tdalign="L";
+        $this->tdbgcolor=false;
+    }
+    if($tag=='TR') { // TR-END
+        $this->Ln();
+    }
+    if($tag=='TABLE') { // TABLE-END
+        $this->tableborder=0;
+    }
+
+    if($tag=='STRONG')
+        $tag='B';
+    if($tag=='EM')
+        $tag='I';
+    if($tag=='B' || $tag=='I' || $tag=='U')
+        $this->SetStyle($tag,false);
+    if($tag=='A')
+        $this->HREF='';
+    if($tag=='FONT'){
+        if ($this->issetcolor==true) {
+            $this->SetTextColor(0);
+        }
+        if ($this->issetfont) {
+            $this->SetFont('arial');
+            $this->issetfont=false;
+        }
+    }
+}
+
+function SetStyle($tag, $enable)
+{
+    //Modify style and select corresponding font
+    $this->$tag+=($enable ? 1 : -1);
+    $style='';
+    foreach(array('B','I','U') as $s) {
+        if($this->$s>0)
+            $style.=$s;
+    }
+    $this->SetFont('',$style);
+}
+
+function PutLink($URL, $txt)
+{
+    //Put a hyperlink
+    $this->SetTextColor(0,0,255);
+    $this->SetStyle('U',true);
+    $this->Write(5,$txt,$URL);
+    $this->SetStyle('U',false);
+    $this->SetTextColor(0);
+}
+
+function LoadData($file)
+{
+    // Read file lines
+    foreach($file as $line)
+        $data[] = explode(';',trim($line));
+    return $data;
+}
+
+
 // Page header
 function Header()
 {
@@ -83,18 +367,23 @@ function Header()
     $this->Ln(20);
 }
 
-function BasicTable()
+function BasicTable($header, $data)
 {
     // Header
+    $this->MultiCell(0,0,false);
+    $this->setLeftMargin(50);
     foreach($header as $col)
-        $this->Cell(40,7,$col,1);
+        $this->Cell(40,7,$col,1,0,'C');
     $this->Ln();
+    $i = 0;
     // Data
     foreach($data as $row)
     {
-        foreach($row as $col)
-            $this->Cell(40,6,$col,1);
-        $this->Ln();
+        foreach($row as $col){
+            $i++;
+            $this->Cell(40,6,$col,1,0,'C');
+
+        }
     }
 }
 
@@ -109,6 +398,8 @@ function Footer()
     $this->Cell(0,10,'Page '.$this->PageNo(),0,0,'R');
 }
 }
+
+
 
 $pdf = new PDF();
 $pdf->AliasNbPages();
@@ -155,9 +446,73 @@ $pdf->Cell(0,64,'2nd : ' . $result['second_course'],0,0,'L');
 $pdf->MultiCell(0,0,false);
 $pdf->Cell(0,50,'Preferred Courses',0,1,'L');
 $pdf->SetFont('Arial','B',10);
-$pdf->Cell(0,-25,'ENTRACE EXAM RATING',0,0,'L');
+$pdf->Cell(0,-25,'ENTRANCE EXAM RATING',0,0,'L');
+$pdf->MultiCell(0,0,false);
+$pdf->Ln(-8);
+$pdf->setLeftMargin(35);
+$html='<table border="1" >
+<tr>
+<td width="200" height="30">                  TOTAL</td>
+<td width="200" height="30">                Raw Score</td>
+<td width="200" height="30">      Descriptive Equivalent</td>
+</tr>
+<tr>
+<td width="200">&nbsp;</td>
+<td width="200">                        '.$result['over_all_total'].'</td>
+<td width="200">(DATA HERE)</td>
+</tr>
+<tr>
+<td width="200" height="30">               VERBAL</td><td width="200" height="30">                        '.$result['verbal_total'].'</td>
+<td width="200" height="30">(DATA HERE)</td>
+</tr>
+<tr>
+<td width="200" height="30">Verbal Comprehension</td>
+<td width="200" height="30">                        '.$result['verbal_comprehension'].'</td>
+<td width="200" height="30">(DATA HERE)</td>
+</tr>
+<tr>
+<td width="200" height="30">Verbal Reasoning</td>
+<td width="200" height="30">                        '.$result['verbal_reasoning'].'</td>
+<td width="200" height="30">(DATA HERE)</td>
+</tr>
+
+<tr>
+<td width="200" height="30">            NON VERBAL</td><td width="200" height="30">
+                       '.$result['non_verbal_total'].'</td>
+<td width="200" height="30">(DATA HERE)</td>
+</tr>
+<tr>
+<td width="200" height="30">Figurative Reasoning</td>
+<td width="200" height="30">                        '.$result['figurative_reasoning'].'</td>
+<td width="200" height="30">(DATA HERE)</td>
+</tr>
+<tr>
+<td width="200" height="30">Quantitative Reasoning</td>
+<td width="200" height="30">                        '.$result['quantitative_reasoning'].'</td>
+<td width="200" height="30">(DATA HERE)</td>
+</tr>
 
 
-
+</table>';
+$pdf->WriteHTML($html);
+$pdf->SetFont('Arial','',7);
+$pdf->setLeftMargin(20);
+$pdf->setRightMargin(24);
+$pdf->setRightMargin(24);
+$pdf->Ln(20);
+$pdf->Cell(0,0,'Date Printed: ' . date('d/m/Y h:i A',time()),0,0,'L');
+$pdf->SetFont('Arial','',10);
+$pdf->Line(113,162,190,162);
+$pdf->Cell(0,0,$result['guidance_counselor'],0,0,'R');
+$pdf->SetFont('Arial','',9);
+$pdf->setRightMargin(40);
+$pdf->Cell(0,11,'Guidance Counselor III',0,0,'R');
+$pdf->setLeftMargin(20);
+$pdf->Ln(10.5);
+$pdf->SetFont('Arial','I',7);
+$pdf->SetTextColor(255,0,0);
+$pdf->Cell(0,0,'NOT VALID IF THERE IS ANY ALTERATION',0,0,'L');
+$pdf->setRightMargin(20);
+$pdf->Cell(0,0,'VALID FOR ' . date('Y')  . ' - ' . date('Y',strtotime("+ 1 year")) . ' ONLY',0,0,'R');
 $pdf->Output();
 ?>
