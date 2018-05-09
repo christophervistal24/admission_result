2065f3dccd0c5ebea86cf9d70b164579d46b57ef
Christopher <christophervistal24@gmail.com>
2018-05-09 17:37:24 +0800

    Basic Table Print

--- App/Views/admin/print.php
+++ App/Views/admin/print.php
@@ -30,90 +30,134 @@ ON
 WHERE
     admission_result.id = ' $_GET[id] '
 ")->fetch(PDO::FETCH_ASSOC);
-echo '<pre>';
-print_r($result);
-echo '</pre>';
-//  class PDF extends FPDF
-// {
-// // Page header
-// function Header()
-// {
-//     // Logo
-//     $this->Image($_SERVER['DOCUMENT_ROOT'] . '/system/assets/img/photos/sdssu.png',9,5,26);
-//     // Arial bold 15
-//     $this->SetFont('Arial','B',15);
-//     // Move to the right
-//     $this->Cell(80);
-//     // Title
-//     $this->MultiCell(0,0,false);
-//     $this->Cell(0,0,'SURIGAO DEL SUR STATE UNIVERSITY',0,1,'C');
+ class PDF extends FPDF
+{
+// Page header
+function Header()
+{
+    global $title;
+    // Logo
+    $this->Image($_SERVER['DOCUMENT_ROOT'] . '/system/assets/img/photos/sdssu.png',9,5,26);
+    // Arial bold 15
+    $this->SetFont('Arial','B',15);
+    // Move to the right
+    $this->Cell(80);
+    // Title
+    $this->MultiCell(0,0,false);
+    $this->Cell(0,0,'SURIGAO DEL SUR STATE UNIVERSITY',0,1,'C');
 
-//     $this->SetFont('Arial','',10);
-//     $this->Cell(0,9,'Guidance Center',0,0,'C');
+    $this->SetFont('Arial','',10);
+    $this->Cell(0,9,'Guidance Center',0,0,'C');
 
-//     $this->SetFont('Arial','B',10);
-//     $this->Cell(-20,0,'Reference Code',0,0,'C');
-//     $this->Cell(19,7,'FM-GC-005E',0,0,'C');
+    $this->SetFont('Arial','B',10);
+    $this->Cell(-20,0,'Reference Code',0,0,'C');
+    $this->Cell(19,7,'FM-GC-005E',0,0,'C');
 
-//     $this->MultiCell(0,0,false);
-//     $this->SetFont('Arial','',10);
-//     $this->Cell(359,14,'Revision Number',0,0,'C');
+    $this->MultiCell(0,0,false);
+    $this->SetFont('Arial','',10);
+    $this->Cell(359,14,'Revision Number',0,0,'C');
 
-//     $this->MultiCell(0,0,false);
-//     $this->SetFont('Arial','B',10);
-//     $this->Cell(359,21,'000',0,0,'C');
+    $this->MultiCell(0,0,false);
+    $this->SetFont('Arial','B',10);
+    $this->Cell(359,21,'000',0,0,'C');
 
-//     $this->MultiCell(0,0,false);
-//     $this->SetFont('Arial','',10);
-//     $this->Cell(359,28,'Date Effective',0,0,'C');
+    $this->MultiCell(0,0,false);
+    $this->SetFont('Arial','',10);
+    $this->Cell(359,28,'Date Effective',0,0,'C');
 
-//     $this->MultiCell(0,0,false);
-//     $this->SetFont('Arial','B',10);
-//     $this->Cell(359,35,date('m/d/Y',time()),0,0,'C');
+    $this->MultiCell(0,0,false);
+    $this->SetFont('Arial','B',10);
+    $this->Cell(359,35,date('m/d/Y',time()),0,0,'C');
 
 
-//     $this->SetFont('Arial','',10);
-//     $this->MultiCell(0,0,false);
-//     $this->Cell(0,17,'Tel. No. (086)214-2735',0,0,'C');
-//     $this->MultiCell(0,0,false);
-//     $this->Cell(0,25,'Tandag City, Surigal del Sur',0,0,'C');
-//     $this->MultiCell(0,0,false);
-//     $this->SetTextColor(135,206,250);
-//     $this->Cell(0,33,'www.sdssu.edu.ph',0,0,'C');
-//     $this->Cell(0,33,'','','','',false, "www.sdssu.edu.ph");
-//     // Line break
-//     $this->Ln(20);
-// }
+    $this->SetFont('Arial','',10);
+    $this->MultiCell(0,0,false);
+    $this->Cell(0,17,'Tel. No. (086)214-2735',0,0,'C');
+    $this->MultiCell(0,0,false);
+    $this->Cell(0,25,'Tandag City, Surigal del Sur',0,0,'C');
+    $this->MultiCell(0,0,false);
+    $this->SetTextColor(135,206,250);
+    $this->Cell(0,33,'www.sdssu.edu.ph',0,0,'C');
+    $this->Cell(0,33,'','','','',false, "www.sdssu.edu.ph");
+    // Line break
+    $this->Ln(20);
+}
 
-// // Page footer
-// function Footer()
-// {
-//     // Position at 1.5 cm from bottom
-//     $this->SetY(-15);
-//     // Arial italic 8
-//     $this->SetFont('Arial','I',8);
-//     // Page number
-//     $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
-// }
-// }
+function BasicTable()
+{
+    // Header
+    foreach($header as $col)
+        $this->Cell(40,7,$col,1);
+    $this->Ln();
+    // Data
+    foreach($data as $row)
+    {
+        foreach($row as $col)
+            $this->Cell(40,6,$col,1);
+        $this->Ln();
+    }
+}
 
-// $pdf = new PDF();
-// $pdf->AliasNbPages();
-// $pdf->AddPage();
+// Page footer
+function Footer()
+{
+    // Position at 1.5 cm from bottom
+    $this->SetY(-15);
+    // Arial italic 8
+    $this->SetFont('Arial','I',8);
+    // Page number
+    $this->Cell(0,10,'Page '.$this->PageNo(),0,0,'R');
+}
+}
 
-// $pdf->SetFont('Arial','B',10);
-// $pdf->MultiCell(0,0,false);
-// $pdf->Cell(0,8,'UNIVERSITY ADMISSION TEST RESULT',0,1,'C');
+$pdf = new PDF();
+$pdf->AliasNbPages();
+$title = 'Admission Result';
+$pdf->SetTitle($title);
+$pdf->AddPage();
+$pdf->SetFont('Arial','B',10);
+$pdf->MultiCell(0,0,false);
+$pdf->Cell(0,8,'UNIVERSITY ADMISSION TEST RESULT',0,1,'C');
 
-// $pdf->SetFont('Arial','I',9);
-// $pdf->MultiCell(0,0,false);
-// $pdf->Cell(0,0,'1st Semester AY ' . date('Y')  . ' - ' . date('Y',strtotime("+ 1 year")),0,1,'C');
+$pdf->SetFont('Arial','I',9);
+$pdf->MultiCell(0,0,false);
+$pdf->Cell(0,0,'1st Semester AY ' . date('Y')  . ' - ' . date('Y',strtotime("+ 1 year")),0,1,'C');
 
-// $pdf->SetFont('Arial','',9);
-// $pdf->Cell(0,30,'Sample nameeeeeeeeeeee',0,1,'C');
 
-// $pdf->Line(20,55,190,55);
-// $pdf->SetFont('Arial','',9);
-// $pdf->Cell(40,40,'Last Name',0,1,'C');
-// $pdf->Output();
+$pdf->SetFont('Arial','',9);
+$pdf->Line(20,55,190,55);
+$pdf->Cell(40,30,ucfirst($result['lastname']),0,0,'C');
+$pdf->Cell(110,30,ucfirst($result['firstname']),0,0,'C');
+$pdf->Cell(45,30,ucfirst($result['middlename']),0,0,'C');
+$pdf->MultiCell(0,0,false);
+$pdf->Cell(40,40,'Last Name',0,0,'C');
+$pdf->Cell(110,40,'Firstname ',0,0,'C');
+$pdf->Cell(25,40,'M.I',0,1,'R');
+
+$pdf->SetFont('Arial','',9);
+$pdf->Ln(-22);
+$pdf->Cell(40,23,ucfirst($result['sex']),0,0,'C');
+$pdf->Cell(115,23,ucfirst($result['age']),0,0,'C');
+$pdf->Cell(23,23,ucfirst($result['birthdate']),0,0,'C');
+$pdf->MultiCell(0,0,false);
+$pdf->Line(20,69,40,69);
+$pdf->Line(95,69,119,69);
+$pdf->Line(160,69,190,69);
+$pdf->Cell(40,30,'Sex',0,0,'C');
+$pdf->Cell(115,30,'Age',0,0,'C');
+$pdf->Cell(19,30,'Birthdate',0,0,'R');
+
+$pdf->MultiCell(0,0,false);
+$pdf->setLeftMargin(20);
+$pdf->Cell(0,57,'1st  : ' . $result['first_course'],0,0,'L');
+$pdf->MultiCell(0,0,false);
+$pdf->Cell(0,64,'2nd : ' . $result['second_course'],0,0,'L');
+$pdf->MultiCell(0,0,false);
+$pdf->Cell(0,50,'Preferred Courses',0,1,'L');
+$pdf->SetFont('Arial','B',10);
+$pdf->Cell(0,-25,'ENTRACE EXAM RATING',0,0,'L');
+
+
+
+$pdf->Output();
 ?>
